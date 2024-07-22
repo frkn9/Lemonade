bfa75c7d04cddd25a68f4f557503c09b2ca78a2a
furkan <frkn9@hotmail.com>
2024-07-23 00:00:15 +0300

    initial commit

--- app/src/main/java/com/example/lemonado/MainActivity.kt
+++ app/src/main/java/com/example/lemonado/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
